f48176ba9db0f851b6465a823d641b2a76d254fb
Taylor Peat <taylorpeat@hotmail.com>
2018-04-19 22:38:37 -0400

    Allow screen side switching

--- init.lua
+++ init.lua
@@ -26,6 +26,27 @@ end)
 
 --Resize focused window--
 
+--slide right--
+hs.hotkey.bind({"cmd", "ctrl"}, "Left", function()
+  local win = hs.window.frontmostWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x
+  win:setFrame(f)
+end)
+
+hs.hotkey.bind({"cmd", "ctrl"}, "Right", function()
+  local win = hs.window.frontmostWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+  
+  f.x = max.x + max.w - f.w
+  win:setFrame(f)
+end)
+
 hs.hotkey.bind({"cmd", "alt"}, "Left", function()
   local win = hs.window.frontmostWindow()
   local f = win:frame()
